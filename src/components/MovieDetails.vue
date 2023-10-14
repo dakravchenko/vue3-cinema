402a7cb657f86f8916fd40b78fcdefbf49f2e2c8
Daniil Kravchenko <daniil.kravchenko.92@yandex.ru>
2023-10-14 13:00:08 +0200

    move styles from MovieDetailsView to MovieDetail

--- src/components/MovieDetails.vue
+++ src/components/MovieDetails.vue
@@ -29,5 +29,42 @@ export default {
 </script>
 
 <style>
+.movie-details {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #f7f7f7;
+}
 
+.movie-details-header {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+h1 {
+  font-size: 24px;
+  margin: 0;
+}
+
+.tagline {
+  font-style: italic;
+  color: #555;
+}
+
+.movie-poster img {
+  max-width: 100%;
+  height: auto;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.movie-info {
+  background-color: #fff;
+  padding: 15px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+strong {
+  font-weight: bold;
+}
 </style>
