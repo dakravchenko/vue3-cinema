7290ecff5a22ec092a848e10670cebbcfc16a1dd
Daniil Kravchenko <daniil.kravchenko.92@yandex.ru>
2023-10-15 15:15:52 +0200

    update pager componet, fix bugs related to current page update

--- src/components/PagerComponent.vue
+++ src/components/PagerComponent.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="pager-wrapper" v-if="pageCount">
-    <div class="prev-btn">Prev</div>
+    <button @click="() => currentPage--" class="prev-btn" :disabled="currentPage === 1">Prev</button>
     <ul class="buttons">
-      <li v-for="buttonValue in buttonsValues" :key="buttonValue" 
+      <li @click="() => currentPage = buttonValue" v-for="buttonValue in buttonsValues" :key="buttonValue" 
       :class="{'active-button': buttonValue === currentPage }"
       >{{ buttonValue }}
     </li>
     </ul>
-    <div class="next-btn">Next</div>
+    <button @click="() => currentPage++" class="next-btn" :disabled="currentPage === pageCount">Next</button>
   </div>
 </template>
 
@@ -23,24 +23,31 @@ export default {
       }
     },
     methods: {
-      calculateValuesOfButtons(){
-        this.buttonsValues = []
-        if(this.pageCount <= 5 || this.currentPage === 1){
-          for (let x = 1; x <= this.pageCount || x <= this.currentPage + 4 ; x++){
-            this.buttonsValues.push(x)
-          }
-        } else if(this.currentPage >= 3 && this.pageCount >= 6){
-          this.buttonsValues.push(1)
-          this.buttonsValues.push(this.currentPage)
-          for (let x = this.currentPage; x <= this.currentPage + 3 || x <= this.pageCount; x++){
-            this.buttonsValues.push(x)
-          }
-        }
-      
-      }
+      calculateValuesOfButtons() {
+  this.buttonsValues = []
+  const currentPage = this.currentPage
+  const maxButtons = 5
+
+  if (this.pageCount <= maxButtons) {
+    for (let x = 1; x <= this.pageCount; x++) {
+      this.buttonsValues.push(x)
+    }
+  } else if (this.pageCount > maxButtons && currentPage === 1) {
+    for (let x = 1; x <= maxButtons; x++) {
+      this.buttonsValues.push(x)
+    }
+  } else if (this.pageCount > maxButtons && currentPage !== 1) {
+    this.buttonsValues.push(1)
+    this.buttonsValues.push(currentPage)
+    for (let x = currentPage + 1; x <= currentPage + 3 && x <= this.pageCount; x++) {
+      this.buttonsValues.push(x)
+    }
+  }
+}
     },
-    mounted() {
+    updated() {
       this.calculateValuesOfButtons()
+      console.log(this.currentPage)
     }
     }
 </script>
@@ -88,5 +95,10 @@ export default {
 .buttons li:hover {
   background-color: #aba2a2;
 
+}
+button:disabled {
+  background-color: #c2adad;
+  cursor: not-allowed;
+  pointer-events: none;
 }
 </style>
