16e4a0080879d1fad4508366341a5d8d1e15f3c5
Daniil Kravchenko <daniil.kravchenko.92@yandex.ru>
2023-10-15 12:16:00 +0200

    calculateValuesOfButtons to display btns

--- src/components/PagerComponent.vue
+++ src/components/PagerComponent.vue
@@ -1,6 +1,10 @@
 <template>
-  <div v-if="pageCount">
-    {{ pageCount }}
+  <div class="pager-wrapper" v-if="pageCount">
+    <div class="prev-btn"></div>
+    <ul class="buttons">
+      <li v-for="buttonValue in buttonsValues" :key="buttonValue">{{ buttonValue }}</li>
+    </ul>
+    <div class="next-btn"></div>
   </div>
 </template>
 
@@ -8,8 +12,34 @@
 export default {
     props: {
         pageCount : Number
+    },
+    data(){
+      return {
+        currentPage : 1,
+        buttonsValues : []
+      }
+    },
+    methods: {
+      calculateValuesOfButtons(){
+        this.buttonsValues = []
+        if(this.pageCount <= 5 || this.currentPage === 1){
+          for (let x = 1; x <= this.pageCount || x <= this.currentPage + 4 ; x++){
+            this.buttonsValues.push(x)
+          }
+        } else if(this.currentPage >= 3 && this.pageCount >= 6){
+          this.buttonsValues.push(1)
+          this.buttonsValues.push(this.currentPage)
+          for (let x = this.currentPage; x <= this.currentPage + 3 || x <= this.pageCount; x++){
+            this.buttonsValues.push(x)
+          }
+        }
+        console.log(this.buttonsValues)
+      }
+    },
+    mounted() {
+      this.calculateValuesOfButtons()
     }
-}
+    }
 </script>
 
 <style>
