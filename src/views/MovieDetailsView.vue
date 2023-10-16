be993a918173f07757ec298348ee7e2dfd29b2ff
Daniil Kravchenko <daniil.kravchenko.92@yandex.ru>
2023-10-16 16:25:02 +0200

    using loader in MovieDetailsVue

--- src/views/MovieDetailsView.vue
+++ src/views/MovieDetailsView.vue
@@ -1,4 +1,5 @@
 <template>
+  <LoaderComponent v-if="isLoading"/>
   <div class="movie-details">
     <ErrorDisplayingMovies :error="error" />
     <MovieDetails :movie="movie" />
@@ -10,27 +11,31 @@ import { onMounted, ref } from 'vue';
 import getFilmsById from '../composables/getFilmById';
 import MovieDetails from '../components/MovieDetails.vue';
 import ErrorDisplayingMovies from '../components/ErrorDisplayingMovies.vue';
+import LoaderComponent from '../components/LoaderComponent.vue';
 
 export default {
   name: 'MovieDetailsView',
-  components: { MovieDetails, ErrorDisplayingMovies },
+  components: { MovieDetails, ErrorDisplayingMovies, LoaderComponent },
   props: ['id'],
   setup(props) {
     const movie = ref(null);
     const error = ref(null);
+    const isLoading = ref(false);
 
     const fetchMovie = async () => {
+      isLoading.value= true
       const { data, errorMessage } = await getFilmsById(props.id);
       if (errorMessage) {
         error.value = errorMessage;
       } else {
         movie.value = data;
       }
+      isLoading.value= false
     };
 
     onMounted(fetchMovie);
 
-    return { movie, error };
+    return { movie, error, isLoading };
   },
 };
 </script>
