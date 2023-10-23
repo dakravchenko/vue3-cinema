00c620202178b5e61d9dd98ea9ae4e85d18d98ac
Daniil Kravchenko <daniil.kravchenko.92@yandex.ru>
2023-10-23 17:06:48 +0200

    nav updated using , hamburger menu

--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -1,50 +1,66 @@
 <template>
-    <nav class="navbar">
+  <nav class="navbar">
+    <router-link to="/" >
       <div class="logo">
         <img src="../assets/logo.png" alt="logo">
       </div>
-      <div class="menu">
-        <ul class="nav-menu">
-          <li class="nav-item nav-item-first">
-            <router-link to="/" class="nav-link">Homepage</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/discover" class="nav-link">Discover</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/top-movies" class="nav-link">Top</router-link>
-          </li>
-        </ul>
-      </div>
-      <div class="hamburger">
-        <span class="bar"></span>
-        <span class="bar"></span>
-        <span class="bar"></span>
-      </div>
-    </nav>
-  </template>
+    </router-link>
+    <div class="menu">
+      <ul class="nav-menu" :class="{ active: isMenuOpen }">
+        <li class="nav-item nav-item-first">
+          <router-link to="/" class="nav-link">Homepage</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/discover" class="nav-link">Discover</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/top-movies" class="nav-link">Top</router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="hamburger" >
+      <span class="bar"></span>
+      <span class="bar"></span>
+      <span class="bar"></span>
+    </div>
+  </nav>
+</template>
 
 <script>
 export default {
-//     hamburger.addEventListener("click", () => {
-//     hamburger.classList.toggle("active");
-//     navMenu.classList.toggle("active");
-//   });
+  data() {
+    return {
+      isMenuOpen: false,
+    };
+  },
+  mounted() {
+    const hamburger = this.$el.querySelector(".hamburger");
+    const navMenu = this.$el.querySelector(".nav-menu");
 
-//   document.querySelectorAll(".nav-link").forEach((link) =>
-//     link.addEventListener("click", () => {
-//       hamburger.classList.remove("active");
-//       navMenu.classList.remove("active");
-//     })
-//   );
-}
+    hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      navMenu.classList.toggle("active");
+    });
+
+    const navLinks = this.$el.querySelectorAll(".nav-link");
+    navLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+      });
+    });
+  },
+};
 </script>
 
-<style>
-header {
-  height: 100px;
-  margin-top: 20px;
-  border-bottom: 1px solid black;
+<style scoped>
+
+.navbar {
+  min-height: 70px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-right: 100px;
 }
 
 .logo {
@@ -53,6 +69,14 @@ header {
   height: 35px;
   cursor: pointer;
 }
+
+.nav-menu {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 60px;
+}
+
 li {
   list-style: none;
 }
@@ -64,25 +88,13 @@ a {
   font-weight: 400;
   font-family: "Inter";
 }
-.navbar {
-  min-height: 70px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-right: 100px;
-}
-.nav-menu {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  gap: 60px;
-}
 
 .hamburger {
   display: none;
   cursor: pointer;
   margin: 0 30px;
 }
+
 .bar {
   display: block;
   width: 50px;
@@ -92,26 +104,28 @@ a {
 }
 
 @media (max-width: 768px) {
-  header {
-    border: none;
-  }
   .hamburger {
     display: block;
   }
+
   .hamburger.active .bar:nth-child(2) {
     opacity: 0;
   }
+
   .hamburger.active .bar:nth-child(1) {
     transform: translateY(11px) rotate(45deg);
     transition: 0.5s;
   }
+
   .hamburger.active .bar:nth-child(3) {
     transform: translateY(-11px) rotate(-45deg);
     transition: 0.5s;
   }
+
   .navbar {
     margin-right: 0;
   }
+
   .nav-menu {
     min-height: 100vh;
     position: fixed;
@@ -128,16 +142,20 @@ a {
   .nav-item {
     margin-top: 30px;
   }
+
   .nav-item-first {
     margin-top: 50px;
   }
+
   .nav-link {
     font-size: 30px;
   }
+
   .nav-menu.active {
     left: 0;
     background: linear-gradient(270deg, #90cea1 0%, #01b4e4 100%);
   }
+
   a {
     color: white;
     font-size: 24px;
